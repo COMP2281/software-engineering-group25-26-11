989d2ddc4887d267cbfe426a46f0d0d3129f28cf
Joseph Garnett <jgarnett370@gmail.com>
2025-12-02 20:28:28 +0000

    create StartModeController - toggle between colour selectiona and painting mode. Usage - use inspector find StartModeController - select / deselect Start in Painting Mode

---
+++ SE-CW-Unity/Assets/Scripts/StartModeController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class StartModeController : MonoBehaviour
+{
+    public UIManager uiManager;
+    public PaintballManager paintballManager;
+
+    public bool startInPaintingMode = false;
+
+    // Default colours automatically added when skipping
+    public List<ColorButton> defaultColorButtons;
+
+   void Start()
+    {
+        if (uiManager == null || paintballManager == null)
+        {
+            Debug.LogWarning("StartModeController missing references.");
+            return;
+        }
+
+        if (!startInPaintingMode)
+        {
+            uiManager.SetInitialState();
+            return;
+        }
+
+        // Simulate spawning balls from color buttons
+        foreach (var button in defaultColorButtons)
+        {
+            button.SpawnBall();
+        }
+
+        // Pretend like you pressed the submit button
+        uiManager.OnSubmit();
+    }
+
+
+}
